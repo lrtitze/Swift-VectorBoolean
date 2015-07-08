9fce61c94aaaf398b20953f012c344e22ce74e9a
Leslie Titze <lrtitze@mac.com>
2015-07-07 23:02:06 -0500

    Initial Commit

---
+++ Swift VectorBoolean/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift VectorBoolean
+//
+//  Created by Leslie Titze on 2015-07-07.
+//  Copyright (c) 2015 Startside Softworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
