63f42c9ad294285c59fe6e620b9da5c7dd2e7b94
Leslie Titze <lrtitze@mac.com>
2015-07-24 13:43:42 -0500

    Oops

--- Swift VectorBooleanTests/FBGeometryTests.swift
+++ Swift VectorBooleanTests/FBGeometryTests.swift
@@ -13,7 +13,7 @@ class FBGeometryTests: XCTestCase {
 
   override func setUp() {
     super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.FBDistanceBetweenPoints
+    // Put setup code here. This method is called before the invocation of each test method in the class.
   }
 
   override func tearDown() {
