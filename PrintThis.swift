14db25e827c84f9db9582164dd2c21589feb93f7
Leslie Titze <lrtitze@mac.com>
2015-08-07 19:11:12 -0500

    Removed my temp print file as I found a better way to print pieces of the code.

--- PrintThis.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PrintThis.swift
-//  Swift VectorBoolean
-//
-//  Created by Leslie Titze on 2015-07-25.
-//  Copyright (c) 2015 Startside Softworks. All rights reserved.
-//
-
-import Foundation
